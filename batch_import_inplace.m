9323932eb1692b581a90384c06d65865b6b51f63
Balaji Ramalingam <b.ramalingam@dundee.ac.uk>
2014-02-20 12:07:39 +0000

    using importCandidates instead of importImage, creating duplicate candidate with specific paths within loop.

--- batch_import_inplace.m
+++ batch_import_inplace.m
@@ -63,69 +63,68 @@ nameFolds= setdiff(nameFolds,{'.','..'});
 %Looped Import
 
 for i=[2 3 24]
-    
-    tic;Projectname = nameFolds{i};
-    project = createProject(session, Projectname);t1=toc; %Create a project
-    
-    
-    %Metadatastore Object
-    tic;store = config.createStore();    
-    store.logVersionInfo(config.getIniVersionNumber());t2=toc;
-    tic;reader = OMEROWrapper(config);t3=toc;
-    
-    diary('log_test.txt')
-    
-    paths = [pathFolder Projectname];
-    tic;handler = ErrorHandler(config);t4=toc;    
-    tic;candidates = ImportCandidates(reader, paths, handler);t5=toc;
-    
-    %Check point 1 : to check if its a SWP(screen/well/plate format)
-    check_spw = candidates.getContainers().get(0).getIsSPW();
-    if check_spw.toString.matches('true')
-        continue
-    end
-    
-    datasetno=candidates.getContainers().size;
-    timevec=[];errorvec={};
-    
-    candidates_dup=candidates;
-    %calculate the files for import
-    getfiles=[];dataID=[];
-    for j=1:datasetno
-        
-        
-        getfiles=[getfiles ; cell(candidates.getContainers().get(j-1).getUsedFiles)]; %#ok<*AGROW>
-        getfile=char(candidates.getContainers.get(j-1).getFile);
-        idx1 = strfind(getfile,'/');
-        DatasetName = strdiff(getfile(1:idx1(end)-1),[paths '/']);%Slash (/) format for mac as of now, can be generalized later.
-        
-        if isempty(DatasetName)
-            DatasetName = getfile(idx1(end+1):length(getfile));            
-        end
-        tic;dataset = createDataset(session, DatasetName, project.getId.getValue());t6=toc;
-        dataID = javaObject('java.lang.Long',dataset.getId().getValue());
-        
-        %Library Object
-        tic;
-        if importopt == 1
-            library = ImportLibrary(store, reader, SymlinkFileTransfer);
-            else
-            library = ImportLibrary(store, reader);            
-        end;t7=toc;
-        
-        tic;library.addObserver(LoggingImportMonitor());t8=toc;
-        tic;reader.setMetadataOptions(DefaultMetadataOptions(MetadataLevel.ALL));t9=toc;
-        
-        tic;log = org.apache.commons.logging.LogFactory.getLog('ome.formats.importer.ImportLibrary');
-        log.setLevel(0);t10=toc;
-        
-        tic;config.targetId.set(dataID);
-        success = library.importImage(config, candidates.getContainers.get(j-1).getFile);t11=toc;
-        timevec=[timevec ; i datasetno t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11];
-                
-    end
-    finvec=[finvec ; timevec];
-    
+
+tic;Projectname = nameFolds{i};
+project = createProject(session, Projectname);t1=toc; %Create a project
+
+
+%Metadatastore Object
+tic;store = config.createStore();
+store.logVersionInfo(config.getIniVersionNumber());t2=toc;
+tic;reader = OMEROWrapper(config);t3=toc;
+
+diary('log_test.txt')
+
+paths = [pathFolder Projectname];
+tic;handler = ErrorHandler(config);t4=toc;
+tic;candidates = ImportCandidates(reader, paths, handler);t5=toc;
+
+%Check point 1 : to check if its a SWP(screen/well/plate format)
+check_spw = candidates.getContainers().get(0).getIsSPW();
+if check_spw.toString.matches('true')
+continue
+end
+
+datasetno=candidates.getContainers().size;
+timevec=[];errorvec={};
+
+%calculate the files for import
+getfiles=[];dataID=[];
+for j=1:datasetno
+
+
+getfiles=[getfiles ; cell(candidates.getContainers().get(j-1).getUsedFiles)]; %#ok<*AGROW>
+getfile=char(candidates.getContainers.get(j-1).getFile);
+idx1 = strfind(getfile,'/');
+DatasetName = strdiff(getfile(1:idx1(end)-1),[paths '/']);%Slash (/) format for mac as of now, can be generalized later.
+
+if isempty(DatasetName)
+DatasetName = getfile(idx1(end+1):length(getfile));
+end
+tic;dataset = createDataset(session, DatasetName, project.getId.getValue());t6=toc;
+dataID = javaObject('java.lang.Long',dataset.getId().getValue());
+
+%Library Object
+tic;
+if importopt == 1
+library = ImportLibrary(store, reader, SymlinkFileTransfer);
+else
+library = ImportLibrary(store, reader);
+end;t7=toc;
+
+tic;library.addObserver(LoggingImportMonitor());t8=toc;
+tic;reader.setMetadataOptions(DefaultMetadataOptions(MetadataLevel.ALL));t9=toc;
+
+tic;log = org.apache.commons.logging.LogFactory.getLog('ome.formats.importer.ImportLibrary');
+log.setLevel(0);t10=toc;
+tic;config.targetId.set(dataID);
+candidates_specific= ImportCandidates(reader, char(candidates.getContainers.get(j-1).getFile), handler);
+success = library.importCandidates(config, candidates_specific);t11=toc;
+timevec=[timevec ; i datasetno t1 t2 t3 t4 t5 t6 t7 t8 t9 t10 t11];
+
+end
+finvec=[finvec ; timevec];
+
 end
 %Logout and close session
 store.logout();
